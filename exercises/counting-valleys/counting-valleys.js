7e13236553368811ed051c2484d549f044bdb58c
greg-byrne <greg.byrne@ie.ibm.com>
2019-09-11 20:08:22 +0100

    update to counting valleys solution

--- exercises/counting-valleys/counting-valleys.js
+++ exercises/counting-valleys/counting-valleys.js
@@ -1,17 +1,20 @@
 module.exports = function countingValleys(n, s) {
-  const journey = s.split("");
+  const journey = s;
+  const totalSteps = n;
   let altitude = 0;
   let valleys = 0;
-  for (var step = 0; step < journey.length; step++) {
-    if (journey[step] === "U") {
-      if (altitude === -1) {
-        ++valleys;
-      }
-      altitude++;
-    } else if (journey[step] === "D") {
-      altitude--;
-    }
+  for(var step = 0; step < totalSteps; step++) {
+     
+      if(journey.charAt(step) === 'U') {
+          if(altitude === -1) {
+              ++valleys;
+          }
+          altitude++;
+      } else if(journey.charAt(step) === 'D') {  
+          altitude--;
+      }        
   }
-
+  
   return valleys;
+
 };
